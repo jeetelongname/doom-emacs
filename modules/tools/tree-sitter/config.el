a43baae1bed8a6778f92dd323042b5ba33e01f87
Jeetaditya Chatterjee <jeetelongname@gmail.com>
2021-08-21 21:19:27 +0100

    module: add :tools tree-sitter
    This module installs 'tree-sitter' and 'tree-sitter-langs' as well as
    the 'evil-textobj-treesitter' and configures them.
    
    This module is based on the prototype module that @hlissner has started
    and I am just fleshing out that work.
    
    It also comes with a README file

---
+++ modules/tools/tree-sitter/config.el
@@ -0,0 +1,32 @@
+;;; ui/tree-sitter/config.el -*- lexical-binding: t; -*-
+
+(use-package! tree-sitter
+  :when (bound-and-true-p module-file-suffix)
+  :hook (prog-mode . tree-sitter-mode)
+  :hook (tree-sitter-after-on . tree-sitter-hl-mode)
+  :config
+  (require 'tree-sitter-langs)
+  (setq tree-sitter-debug-jump-buttons t
+        tree-sitter-debug-highlight-jump-region t)
+
+  (defadvice! doom-tree-sitter-fail-gracefully-a (orig-fn &rest args)
+    "Don't break with errors when current major mode lacks tree-sitter support."
+    :around #'tree-sitter-mode
+    (condition-case e
+        (apply orig-fn args)
+      (error
+       (unless (string-match-p (concat "^Cannot find shared library\\|"
+                                       "^No language registered\\|"
+                                       "cannot open shared object file")
+                               (error-message-string e))
+         (signal (car e) (cadr e)))))))
+
+(use-package! evil-textobj-treesitter
+  :after tree-sitter
+  :config
+  (map!
+   :textobj "f" (evil-textobj-treesitter-get-textobj "function.inner") (evil-textobj-treesitter-get-textobj "function.outer") ;; redef
+   :textobj "C" (evil-textobj-treesitter-get-textobj "class.inner") (evil-textobj-treesitter-get-textobj "class.outer")
+   :textobj "c" nil (evil-textobj-treesitter-get-textobj "comment.outer")
+   :textobj "i" (evil-textobj-treesitter-get-textobj "conditional.inner") (evil-textobj-treesitter-get-textobj "conditional.outer")
+   :textobj "l" (evil-textobj-treesitter-get-textobj "loop.inner") (evil-textobj-treesitter-get-textobj "loop.outer")))
